db34d05575b01fbd2d4a337a6849428335379129
flyicegood <flyicegood@163.com>
2017-12-27 23:34:10 +0800

    modify guid

--- AFINet.cs
+++ AFINet.cs
@@ -12,42 +12,42 @@ using System.Runtime.InteropServices;
 
 namespace AFTCPClient
 {
-    public class ConstDefine
+public class ConstDefine
+{
+    public static UInt32 AF_PACKET_HEAD_SIZE = 22;
+    public static int MAX_PACKET_LEN = 655360;
+};
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public class MsgHead
+{
+    public MsgHead()
     {
-        public static UInt32 AF_PACKET_HEAD_SIZE = 14;
-        public static int MAX_PACKET_LEN = 655360;
-    };
-
-    [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public class MsgHead
-    {
-        public MsgHead()
-        {
-            unMsgID = 0;
-            unDataLen = 0;
-        }
-        public UInt16 unMsgID;
-        public UInt32 unDataLen;
-        public Int32 nHead32;
-        public Int32 nData32;
-    };
-
-    public abstract class AFINet
-    {
-        public delegate void MsgDelegation(MsgHead head, MemoryStream stream);
-        public delegate void ResultCodeDelegation(int eGameEventCode);
-
-        public delegate void OnConnectDelegation();
-        public delegate void OnDisConnectDelegation();
-
-        public abstract void StartConnect(string strIP, int nPort);
-        public abstract void SendMsg(MsgHead head, byte[] bodyByte);
-        public abstract void Update();
-        public abstract void Disconnect();
-        public abstract bool RegisteredDelegation(int eMsgID, MsgDelegation msgDelegate);
-        public abstract bool RegisteredResultCodeDelegation(int eCode, ResultCodeDelegation msgDelegate);
-        public abstract bool RegisteredDisConnectDelegation(OnDisConnectDelegation onDisConnectDelegate);
-        public abstract bool RegisteredConnectDelegation(OnConnectDelegation onConnectDelegate);
-        public abstract bool DoResultCodeDelegation(int eCode);
+        unMsgID = 0;
+        unDataLen = 0;
     }
+    public UInt16 unMsgID;
+    public UInt32 unDataLen;
+    public Int64 nHead64;
+    public Int64 nData64;
+};
+
+public abstract class AFINet
+{
+    public delegate void MsgDelegation(MsgHead head, MemoryStream stream);
+    public delegate void ResultCodeDelegation(int eGameEventCode);
+
+    public delegate void OnConnectDelegation();
+    public delegate void OnDisConnectDelegation();
+
+    public abstract void StartConnect(string strIP, int nPort);
+    public abstract void SendMsg(MsgHead head, byte[] bodyByte);
+    public abstract void Update();
+    public abstract void Disconnect();
+    public abstract bool RegisteredDelegation(int eMsgID, MsgDelegation msgDelegate);
+    public abstract bool RegisteredResultCodeDelegation(int eCode, ResultCodeDelegation msgDelegate);
+    public abstract bool RegisteredDisConnectDelegation(OnDisConnectDelegation onDisConnectDelegate);
+    public abstract bool RegisteredConnectDelegation(OnConnectDelegation onConnectDelegate);
+    public abstract bool DoResultCodeDelegation(int eCode);
+}
 }
